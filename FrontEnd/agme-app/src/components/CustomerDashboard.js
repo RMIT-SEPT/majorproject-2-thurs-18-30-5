357098daac2059866a9cf2df9898102e5cb05088
[Ge Lee s3776409] <s3776409@student.rmit.edu.au>
2020-08-26 01:30:43 +1000

    Implemented customer dashboard with required componenets

---
+++ FrontEnd/agme-app/src/components/CustomerDashboard.js
@@ -0,0 +1,104 @@
+import React, { Component } from 'react'
+import CustomerHeader from './Layout/CustomerHeader'
+import Footer from './Layout/Footer'
+import './CustomerDashboard.css'
+
+export default class CustomerDashboard extends Component {
+  render() {
+    return (
+      <div>
+        <CustomerHeader/>
+        <div className="cust-img">
+          <div className="container customer-title">
+            <div className="welcome-msg">G'day, JOHN!</div>
+            <div className="btn book-btn">Book a service</div>
+          </div>
+          
+          <div className="container upcoming-booking">
+          <div className="upcoming-title">Upcoming bookings</div>
+          <div className="table-wrapper-scroll-y my-custom-scrollbar">
+            <table className="table table-editable text-nowrap table-borderless table-hover">
+              <thead>
+                <tr>
+                  <th scope="col" width="20%">Date</th>
+                  <th scope="col" width="20%">Time</th>
+                  <th scope="col" width="30%">Service</th>
+                  <th scope="col" width="25%">Worker</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <th scope="row">14 Oct 2020</th>
+                  <td>4pm - 6pm</td>
+                  <td>Gym</td>
+                  <td>Mark</td>
+                  <td className="table-remove text-center cancel-booking">
+                    <button type="button" className="close">&times;</button>
+                  </td>
+                </tr>
+                <tr>
+                  <th scope="row">18 Oct 2020</th>
+                  <td>1pm - 2pm</td>
+                  <td>Hairdressing</td>
+                  <td>Sarah</td>
+                  <td className="table-remove text-center cancel-booking">
+                    <button type="button" className="close">&times;</button>
+                  </td>
+                </tr>
+                <tr>
+                  <th scope="row">22 Oct 2020</th>
+                  <td>10am - 11am</td>
+                  <td>Dentist</td>
+                  <td>Peter</td>
+                  <td className="table-remove text-center cancel-booking">
+                    <button type="button" className="close">&times;</button>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+          </div>
+
+          <div className="container past-booking">
+          <div className="past-title">Past bookings</div>
+            <table className="table table-editable text-nowrap table-borderless table-hover">
+              <thead>
+                <tr>
+                  <th scope="col" width="20%">Date</th>
+                  <th scope="col" width="20%">Time</th>
+                  <th scope="col" width="30%">Service</th>
+                  <th scope="col" width="25%">Worker</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <th scope="row">14 May 2020</th>
+                  <td>4pm - 6pm</td>
+                  <td>Gym</td>
+                  <td>Mark</td>
+                  <td className="table-remove text-center cancel-booking"></td>
+                </tr>
+                <tr>
+                  <th scope="row">18 May 2020</th>
+                  <td>1pm - 2pm</td>
+                  <td>Hairdressing</td>
+                  <td>Sarah</td>
+                  <td className="table-remove text-center cancel-booking"></td>
+                </tr>
+                <tr>
+                  <th scope="row">22 May 2020</th>
+                  <td>10am - 11am</td>
+                  <td>Dentist</td>
+                  <td>Peter</td>
+                  <td className="table-remove text-center cancel-booking"></td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+          
+        </div>
+        <Footer/>
+      </div>
+    )
+  }
+}
