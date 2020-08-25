b1940beac33a9a10d788d3d9dbd209fd28a7a865
[Ge Lee s3776409] <s3776409@student.rmit.edu.au>
2020-08-26 01:27:55 +1000

    Removed about & contact us component to new file

--- FrontEnd/agme-app/src/components/LandingPage.js
+++ FrontEnd/agme-app/src/components/LandingPage.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import Footer from './Layout/Footer'
 import Header from './Layout/Header'
+import About_Contact from './Layout/About_Contact'
 import './LandingPage.css'
 
 export default class LandingPage extends Component {
@@ -9,24 +10,7 @@ export default class LandingPage extends Component {
       <div>
         <Header/>
           <div className="img">
-
-            <div className="about-title">About</div>
-            <div className="about-content">
-              Welcome to AGME company's Online Appointment Booking System. 
-              This is a web application booking system that can be used by any service provider, 
-              allowing customers to book 24/7 a time slot for an appointment for a specific service with an assigned service provider. 
-              You can use the application for any business, e.g., a hairdresser, a gym, a dentist and so on.  
-            </div>
-
-            <div className="contact-title">Contact us</div>
-            <div className="contact-content">
-              You can contact us via our email address or phone number.
-              <br/>
-              Email-address: agme@agmecompany.com
-              <br/>
-              Phone-number: +61-123456789
-            </div>
-            <br/>
+            <About_Contact/>
           </div>
         <Footer/>
       </div>
