ad52afabd8c7f534255512372484253c3350f2ae
donald-uren <s3686963@student.rmit.edu.au>
2020-09-03 13:56:28 +1000

    added basic BusinessRepository
    Signed-off-by: Donald U'Ren <s3686963@student.rmit.edu.au>

--- BackEnd/src/main/java/com/rmit/sept/assignment/initial/Repositories/BusinessRepository.java
+++ BackEnd/src/main/java/com/rmit/sept/assignment/initial/Repositories/BusinessRepository.java
@@ -1,4 +1,15 @@
 package com.rmit.sept.assignment.initial.Repositories;
 
-public class BusinessRepository {
+
+import com.rmit.sept.assignment.initial.model.Business;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface BusinessRepository extends CrudRepository<Business, Long> {
+    @Override
+    Optional<Business> findById(Long aLong);
+    List<Business> findAllByName(String name);
+    List<Business> findAllByNameContains(String name);
 }
