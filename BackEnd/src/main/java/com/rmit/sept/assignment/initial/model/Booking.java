6ab59b1d47b61e483d3d14b4e2a5d6fa36f008a6
donald-uren <s3686963@student.rmit.edu.au>
2020-09-03 21:19:49 +1000

    began basic Booking class, added FK mapping to User
    Signed-off-by: Donald U'Ren <s3686963@student.rmit.edu.au>

---
+++ BackEnd/src/main/java/com/rmit/sept/assignment/initial/model/Booking.java
@@ -0,0 +1,106 @@
+package com.rmit.sept.assignment.initial.model;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.Date;
+import java.util.Objects;
+
+@Entity(name = "Booking")
+@Table(name = "booking")
+public class Booking {
+    @Id
+    @GeneratedValue
+    @Column(name = "id")
+    private Long id;
+    @ManyToOne
+    @JoinColumn(name = "worker_id")
+    @JsonIgnoreProperties("bookings")
+    private Worker worker;
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    @JsonIgnoreProperties("bookings")
+    private User user;
+
+    @NotNull(message = "Start date cannot be null")
+    @JsonFormat(pattern = "yyyy-mm-dd hh:mm")
+    private Date start;
+    @NotNull(message = "End date cannot be null")
+    @JsonFormat(pattern = "yyyy-mm-dd hh:mm")
+    private Date end;
+
+    @CreationTimestamp
+    @JsonFormat(pattern = "yyyy-mm-dd hh:mm")
+    private Date createdAt;
+    @UpdateTimestamp
+    @JsonFormat(pattern = "yyyy-mm-dd hh:mm")
+    private Date updatedAt;
+
+    public Booking() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Worker getWorker() {
+        return worker;
+    }
+
+    public void setWorker(Worker worker) {
+        this.worker = worker;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Date getStart() {
+        return start;
+    }
+
+    public void setStart(Date start) {
+        this.start = start;
+    }
+
+    public Date getEnd() {
+        return end;
+    }
+
+    public void setEnd(Date end) {
+        this.end = end;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Booking booking = (Booking) o;
+        return Objects.equals(id, booking.id);
+    }
+}
