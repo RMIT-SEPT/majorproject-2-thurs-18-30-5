af3cd57077828a35114f54340ae2483a51684eac
[Ge Lee s3776409] <s3776409@student.rmit.edu.au>
2020-09-08 02:29:45 +1000

    Removed additional div tag

--- FrontEnd/agme-app/src/components/Layout/CustomerHeader.js
+++ FrontEnd/agme-app/src/components/Layout/CustomerHeader.js
@@ -5,27 +5,25 @@ import './Header.css'
 export default class CustomerHeader extends Component {
 	render() {
     return (
-      <div>
-        <div className="App">
-          <nav className="navbar navbar-expand-lg navbar-light fixed-top">
-            <div className="container">
-							<Link className="navbar-brand" to={"/customer-dashboard"}>AGME</Link>
-							
-              <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
-                <ul className="navbar-nav ml-auto">
-                  <li className="nav-item">
-                    <Link className="nav-link" to={"/customer-profile"}>Profile</Link>
-                  </li>
+      <div className="App">
+        <nav className="navbar navbar-expand-lg navbar-light fixed-top">
+          <div className="container">
+            <Link className="navbar-brand" to={"/customer-dashboard"}>AGME</Link>
+            
+            <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+              <ul className="navbar-nav ml-auto">
+                <li className="nav-item">
+                  <Link className="nav-link profile" to={"/customer-profile"}>Profile</Link>
+                </li>
 
-                  <li className="nav-item">
-                    <Link className="nav-link" to={"/"}>Logout</Link>
-                  </li>
-                </ul>
-							</div>
-							
+                <li className="nav-item">
+                  <Link className="nav-link logout" to={"/"}>Logout</Link>
+                </li>
+              </ul>
             </div>
-          </nav>
-        </div>
+            
+          </div>
+        </nav>
       </div>
     )
   }
--- FrontEnd/agme-app/src/components/Layout/Header.js
+++ FrontEnd/agme-app/src/components/Layout/Header.js
@@ -5,28 +5,26 @@ import './Header.css'
 export default class Header extends Component {
   render() {
     return (
-      <div>
-        <div className="App">
-          <nav className="navbar navbar-expand-lg navbar-light fixed-top">
-            <div className="container">
+      <div className="App">
+        <nav className="navbar navbar-expand-lg navbar-light fixed-top">
+          <div className="container">
 
-              <Link className="navbar-brand" to={"/"}>AGME</Link>
-              <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+            <Link className="navbar-brand" to={"/"}>AGME</Link>
+            <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
 
-                <ul className="navbar-nav ml-auto">
-                  <li className="nav-item">
-                    <Link className="nav-link" to={"/sign-in"}>Login</Link>
-                  </li>
+              <ul className="navbar-nav ml-auto">
+                <li className="nav-item">
+                  <Link className="nav-link sign-in" to={"/sign-in"}>Login</Link>
+                </li>
 
-                  <li className="nav-item">
-                    <Link className="nav-link" to={"/sign-up"}>Sign up</Link>
-                  </li>
-                </ul>
+                <li className="nav-item">
+                  <Link className="nav-link sign-up" to={"/sign-up"}>Sign up</Link>
+                </li>
+              </ul>
 
-              </div>
             </div>
-          </nav>
-        </div>
+          </div>
+        </nav>
       </div>
     )
   }
