3ee6972858d6054f4e432db33046a46e017bd21c
[Ge Lee s3776409] <s3776409@student.rmit.edu.au>
2020-09-17 00:33:33 +1000

    Added header for admin related pages

---
+++ FrontEnd/agme-app/src/components/Layout/AdminHeader.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+import { BrowserRouter as Router, Link } from "react-router-dom"
+import './Header.css'
+
+export default class CustomerHeader extends Component {
+	render() {
+    return (
+      <div>
+        <div className="App">
+          <nav className="navbar navbar-expand-lg navbar-light fixed-top">
+            <div className="container">
+							<Link className="navbar-brand" to={"/admin-dashboard"}>AGME</Link>
+							
+              <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+                <ul className="navbar-nav ml-auto">
+                  <li className="nav-item">
+                    <Link className="nav-link" to={"/"}>Logout</Link>
+                  </li>
+                </ul>
+							</div>
+							
+            </div>
+          </nav>
+        </div>
+      </div>
+    )
+  }
+}
