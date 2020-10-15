6107fe7df7a2c3a2309901a5f482314f102c0de1
donald-uren <s3686963@student.rmit.edu.au>
2020-10-15 20:36:27 +1100

    found issue in BookingRepository query findAllByUser_IdAndStatusOrStatus() which was incorrectly filtering by (id & s1) || s2, and not id & (s1 || s2) added new query to check for (id and s1) or (id and s2)
    Signed-off-by: Donald U'Ren <s3686963@student.rmit.edu.au>

--- BackEnd/src/main/java/com/rmit/sept/assignment/initial/repositories/BookingRepository.java
+++ BackEnd/src/main/java/com/rmit/sept/assignment/initial/repositories/BookingRepository.java
@@ -5,6 +5,7 @@ import com.rmit.sept.assignment.initial.model.User;
 import com.rmit.sept.assignment.initial.model.Worker;
 import org.springframework.data.repository.CrudRepository;
 
+import javax.validation.constraints.NotNull;
 import java.util.Date;
 import java.util.List;
 import java.util.Optional;
@@ -17,14 +18,12 @@ public interface BookingRepository extends CrudRepository<Booking, Long> {
     List<Booking> findAllByUser(User user);
     List<Booking> findAllByUser_Id(Long userId);
     List<Booking> findAllByUser_IdAndStatus(Long userId, Booking.BookingStatus status);
-    List<Booking> findAllByUser_IdAndStartAfter(Long userId, Date date);
     List<Booking> findAllByWorker(Worker worker);
     List<Booking> findAllByWorker_Id(Long workerId);
     List<Booking> findAllByWorker_IdAndStatus(Long workerId, Booking.BookingStatus status);
     List<Booking> findAllByWorker_Business_Id(Long businessId);
     List<Booking> findAllByWorker_Business_IdAndStatus(Long businessId, Booking.BookingStatus status);
-    List<Booking> findAllByStartAfter(Date date);
     List<Booking> findAllByStatus(Booking.BookingStatus status);
-    List<Booking> findAllByWorker_IdAndStatusOrStatus (Long workerId, Booking.BookingStatus status1, Booking.BookingStatus status2);
-    List<Booking> findAllByUser_IdAndStatusOrStatus (Long userId, Booking.BookingStatus status1, Booking.BookingStatus status2);
+    List<Booking> findAllByWorker_IdAndStatusOrWorker_IdAndStatus(@NotNull(message = "Worker ID must be provided") Long worker_id, Booking.BookingStatus status, @NotNull(message = "Worker ID must be provided") Long worker_id2, Booking.BookingStatus status2);
+    List<Booking> findAllByUser_IdAndStatusOrUser_IdAndStatus(@NotNull(message = "User ID must be provided") Long user_id, Booking.BookingStatus status, @NotNull(message = "User ID must be provided") Long user_id2, Booking.BookingStatus status2);
 }
