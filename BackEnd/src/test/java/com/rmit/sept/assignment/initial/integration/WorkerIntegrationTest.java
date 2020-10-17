824d5cb1c80fa225b572d6e9f5a50b8d3b3d8094
donald-uren <s3686963@student.rmit.edu.au>
2020-10-18 00:46:24 +1100

    added WorkerIntegrationTest cases, and fixed WorkerControllerTest auth success
    Signed-off-by: Donald U'Ren <s3686963@student.rmit.edu.au>

--- BackEnd/src/test/java/com/rmit/sept/assignment/initial/integration/WorkerIntegrationTest.java
+++ BackEnd/src/test/java/com/rmit/sept/assignment/initial/integration/WorkerIntegrationTest.java
@@ -1,10 +1,12 @@
-package com.rmit.sept.assignment.initial.web;
+package com.rmit.sept.assignment.initial.integration;
 
 import com.rmit.sept.assignment.initial.model.User;
 import com.rmit.sept.assignment.initial.model.Worker;
+import com.rmit.sept.assignment.initial.repositories.WorkerRepository;
 import com.rmit.sept.assignment.initial.security.JwtResponse;
 import com.rmit.sept.assignment.initial.service.AuthRequestService;
 import com.rmit.sept.assignment.initial.service.WorkerService;
+import com.rmit.sept.assignment.initial.web.WorkerController;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.junit.jupiter.api.BeforeEach;
@@ -16,6 +18,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
@@ -24,6 +27,7 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
 import static org.mockito.Mockito.when;
@@ -37,20 +41,27 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 @SpringBootTest
 @ActiveProfiles("test")
-public class WorkerControllerTest {
+public class WorkerIntegrationTest {
     MockMvc mockMvc;
 
     @Autowired
     WorkerController workerController;
 
-    @MockBean
+    @Autowired
     WorkerService workerService;
 
     @MockBean
     AuthRequestService authRequestService;
+
+    @MockBean
+    WorkerRepository repo;
+
+    @MockBean
+    BCryptPasswordEncoder encoder;
 
     /**
      * List of sample workers
@@ -69,13 +80,14 @@ public class WorkerControllerTest {
 
         when(authRequestService.authWorkerRequest(any(), (Worker) any())).thenReturn(true);
         when(authRequestService.authWorkerRequest(any(), (Long) any())).thenReturn(true);
+        when(encoder.matches(anyString(), anyString())).thenReturn(true);
     }
 
     @Test
     @DisplayName("Test getWorkers")
     void testGetWorkers() throws Exception {
         // Mocking service
-        when(workerService.findAll()).thenReturn(workers);
+        when(repo.findAll()).thenReturn(workers);
 
         mockMvc.perform(get("/api/worker/all").contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
@@ -87,7 +99,7 @@ public class WorkerControllerTest {
     @DisplayName("Test getWorker success")
     void testGetWorkerSuccess() throws Exception {
         // Mocking service
-        when(workerService.findById(1L)).thenReturn(workers.get(0));
+        when(repo.findById(1L)).thenReturn(Optional.ofNullable(workers.get(0)));
 
         mockMvc.perform(get("/api/worker/1").contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
@@ -98,7 +110,7 @@ public class WorkerControllerTest {
     @DisplayName("Test getWorker not found")
     void testGetWorkerFail() throws Exception {
         // Mocking service
-        when(workerService.findById(3L)).thenReturn(null);
+        when(repo.findById(3L)).thenReturn(Optional.empty());
 
         mockMvc.perform(get("/api/worker/3").contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isNotFound());
@@ -108,10 +120,11 @@ public class WorkerControllerTest {
     @DisplayName("Test AuthenticateWorker success")
     void testAuthenticateWorkerSuccess() throws Exception {
         // Mocking service
+//        when(workerService.authenticateWorker(1L, "123Qwe!", false)).thenReturn(workers.get(0));
         Worker temp = workers.get(0);
-        when(workerService.authenticateWorker(1L, "123Qwe!", false)).thenReturn(workers.get(0));
         when(authRequestService.getJwtResponse(temp.getUser().getUsername(), temp.getUser().getPassword()))
                 .thenReturn(new JwtResponse("token", 1L, temp.getUser().getUsername()));
+        when(repo.findByIdAndIsAdmin(1L, false)).thenReturn(Optional.of(temp));
         mockMvc.perform(get("/api/worker/auth/id/1").param("password", "123Qwe!")
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
@@ -122,8 +135,10 @@ public class WorkerControllerTest {
     @DisplayName("Test AuthenticateWorker fail")
     void testAuthenticateWorkerFail() throws Exception {
         // Mocking service
-        when(workerService.authenticateWorker(3L, "randomPass", false)).thenReturn(null);
-
+        Worker temp = workers.get(0);
+        when(authRequestService.getJwtResponse(temp.getUser().getUsername(), temp.getUser().getPassword()))
+                .thenReturn(new JwtResponse("token", 1L, temp.getUser().getUsername()));
+        when(repo.findByIdAndIsAdmin(1L, false)).thenReturn(Optional.of(temp));
         mockMvc.perform(get("/api/worker/auth/id/3").param("password", "randomPass")
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isNotFound());
@@ -133,7 +148,8 @@ public class WorkerControllerTest {
     @DisplayName("Test getWorkerByBusiness success")
     void testGetWorkerByBusinessSuccess() throws Exception {
         // Mocking service
-        when(workerService.findAllByBusiness(1L, false)).thenReturn(workers);
+        when(repo.findAllByBusiness_IdAndIsAdmin(1L, false)).thenReturn(workers);
+        when(repo.findAllByBusiness_Id(1L)).thenReturn(workers);
 
         mockMvc.perform(get("/api/worker/business/1").param("isAdmin", "0")
                 .contentType(MediaType.APPLICATION_JSON))
@@ -143,21 +159,11 @@ public class WorkerControllerTest {
     }
 
     @Test
-    @DisplayName("Test getWorkerByBusiness fail (null)")
-    void testGetWorkerByBusinessNull() throws Exception {
-        // Mocking service
-        when(workerService.findAllByBusiness(2L, false)).thenReturn(null);
-
-        mockMvc.perform(get("/api/worker/business/2").param("isAdmin", "0")
-                .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isBadRequest());
-    }
-
-    @Test
     @DisplayName("Test getWorkerByBusiness fail (empty)")
     void testGetWorkerByBusinessEmpty() throws Exception {
         // Mocking service
-        when(workerService.findAllByBusiness(3L, false)).thenReturn(new ArrayList<Worker>());
+        when(repo.findAllByBusiness_IdAndIsAdmin(3L, false)).thenReturn(new ArrayList<>());
+        when(repo.findAllByBusiness_Id(3L)).thenReturn(new ArrayList<>());
 
         mockMvc.perform(get("/api/worker/business/3").param("isAdmin", "0")
                 .contentType(MediaType.APPLICATION_JSON))
@@ -168,7 +174,7 @@ public class WorkerControllerTest {
     @DisplayName("Test createNewWorker success")
     void testCreateNewWorkerSuccess() throws Exception {
         // Mocking service
-        when(workerService.saveOrUpdateWorker(workers.get(0))).thenReturn(workers.get(0));
+        when(repo.save(workers.get(0))).thenReturn(workers.get(0));
 
         String inputJson = "{\"id\":1}";
         RequestBuilder requestBuilder = MockMvcRequestBuilders
@@ -186,7 +192,7 @@ public class WorkerControllerTest {
     @DisplayName("Test createNewWorker badRequest")
     void testCreateNewWorkerFail() throws Exception {
         // Mocking service
-        when(workerService.saveOrUpdateWorker(null)).thenReturn(null);
+        when(repo.save(workers.get(0))).thenReturn(null);
 
         String inputJson = "{\"id\":1}";
         RequestBuilder requestBuilder = MockMvcRequestBuilders
@@ -204,8 +210,8 @@ public class WorkerControllerTest {
     @DisplayName("Test updateWorker success")
     void testUpdateWorkerSuccess() throws Exception {
         // Mocking service
-        when(workerService.findById(1L)).thenReturn(workers.get(0));
-        when(workerService.saveOrUpdateWorker(workers.get(0))).thenReturn(workers.get(0));
+        when(repo.findById(1L)).thenReturn(Optional.ofNullable(workers.get(0)));
+        when(repo.save(workers.get(0))).thenReturn(workers.get(0));
 
         String inputJson = "{\"id\":1}";
         RequestBuilder requestBuilder = MockMvcRequestBuilders
@@ -223,7 +229,8 @@ public class WorkerControllerTest {
     @DisplayName("Test updateWorker badRequest")
     void testUpdateWorkerFail() throws Exception {
         // Mocking service
-        when(workerService.saveOrUpdateWorker(null)).thenReturn(null);
+        when(repo.findById(1L)).thenReturn(Optional.empty());
+        when(repo.save(workers.get(0))).thenReturn(workers.get(0));
 
         String inputJson = "{\"id\":1}";
         RequestBuilder requestBuilder = MockMvcRequestBuilders
